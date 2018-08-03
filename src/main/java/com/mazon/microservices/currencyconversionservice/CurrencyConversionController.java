0f7194c45c6639d2352978706c07e66cf24b8f2e
Moacir Mazon <moacir.mazon@gmail.com>
2018-08-03 15:30:30 -0300

    Add controller e chamando microservice de exchange currency para calculo do cambio

--- src/main/java/com/mazon/microservices/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/mazon/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -4,8 +4,6 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,9 +12,6 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
-	
-	@Autowired
-	private Environment enviroment;
 	
 	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity){
